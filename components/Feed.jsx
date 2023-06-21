2a9843cef17e0a23ac33003af55e6edb0cbaa2b4
vijayt-dev <vijayatk3@gmail.com>
2023-06-21 13:39:22 +0530

    fix feed issue

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -26,7 +26,7 @@ const Feed = () => {
   const [searchedResults, setSearchedResults] = useState([]);
 
   const fetchPosts = async () => {
-    const response = await fetch("/api/prompt", {cache: 'no-store'});
+    const response = await fetch("/api/prompt", {next: { revalidate: 10 }});
     const data = await response.json();
 
     setAllPosts(data);
