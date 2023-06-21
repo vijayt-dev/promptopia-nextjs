9f77ac6d9541712844835f4d041b4168cbfe6cea
vijayt-dev <vijayatk3@gmail.com>
2023-06-21 17:30:43 +0530

    change text

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       <h1 className="head_text text-center">
         Discover & Share
         <br className="max-md:hidden" />
-        <span className="orange_gradient text-center">Creative Prompts</span>
+        <span className="orange_gradient text-center">Discover Creative Prompts</span>
       </h1>
       <p className="desc text-center">
         Promptopia is an open-source AI prompting tool for modern world to
