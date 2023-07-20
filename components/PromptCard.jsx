23618acdc706ea631c1cf23e682ae833a2163216
vijayt-dev <vijayatk3@gmail.com>
2023-07-20 14:21:12 +0530

    update img to promptcard

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -31,20 +31,14 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
           className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
           onClick={handleProfileClick}
         >
-          <img
-            src={post.creator?.image}
-            width={40}
-            height={40}
-            className="rounded-full object-contain"
-          />
-
-          {/* <Image
+          
+          <Image
             src={post.creator?.image}
             alt="user_image"
             width={40}
             height={40}
             className="rounded-full object-contain"
-          /> */}
+          />
 
           <div className="flex flex-col">
             <h3 className="font-satoshi font-semibold text-gray-900">
